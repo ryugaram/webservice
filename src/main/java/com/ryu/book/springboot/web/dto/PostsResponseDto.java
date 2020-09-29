afa2ca0db1811dff660554462dd60c3fc1636c31
ryugaram <gkfka742@naver.com>
2020-09-29 14:12:19 +0900

    그동안

---
+++ src/main/java/com/ryu/book/springboot/web/dto/PostsResponseDto.java
@@ -0,0 +1,23 @@
+package com.ryu.book.springboot.web.dto;
+
+import com.ryu.book.springboot.domain.posts.Posts;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
+@Getter
+public class PostsResponseDto {
+    private Long id;
+    private String title;
+    private String content;
+    private String author;
+    private LocalDateTime modifiedDate;
+
+    public PostsResponseDto(Posts entity) {
+        this.id = entity.getId();
+        this.title = entity.getTitle();
+        this.content = entity.getContent();
+        this.author = entity.getAuthor();
+        this.modifiedDate=entity.getModifiedDate();
+    }
+}
